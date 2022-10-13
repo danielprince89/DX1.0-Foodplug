0cf840c21218bac3122e3896efab746a1eb825bd
TechDan <rotimiayodeji65@gmail.com>
2022-10-13 13:43:27 +0100

    harmburger started

---
+++ starter/main.js
@@ -0,0 +1,16 @@
+
+
+
+// es 5
+
+// console.log("send me your account number")
+
+// var writeup = "na me be the Don here"
+
+
+
+var harmburgerdiv = document.querySelector(".harmburger")
+console.log(harmburgerdiv)
+function codedtunez() {
+    harmburgerdiv.classList.toggle("showburger")
+}
